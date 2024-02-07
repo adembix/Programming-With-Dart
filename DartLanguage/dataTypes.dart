4858c1c60a737e73345ee9bad2dab76586ad645e
Adem Bouaicha <151208515+adembix@users.noreply.github.com>
2024-02-07 22:58:01 +0100

    Add files via upload

---
+++ DartLanguage/dataTypes.dart
@@ -0,0 +1,18 @@
+import 'dart:ffi';
+
+void main() { // main function
+
+  //Declaration Variables
+  int integerVar = 10;
+  double doubleVar = 10.5;
+  String stringVar = 'ten';
+  bool boolVar = true;
+
+  // dynamic data type
+  dynamic dynamicVar;
+
+  dynamicVar = 'welcome';
+  dynamicVar = 10;
+  dynamicVar = 10.5;
+  dynamicVar = true;
+}
